b44af6f3c90d6ef5ff42d373523daaa029ccff1c
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-04-20 18:08:01 -0400

    Add Fusuma multi-touch gesture configuration and installation script

---
+++ install/terminal/app-fusuma.sh
@@ -0,0 +1,22 @@
+# Fusuma is a multi-touch gesture recognizer for Linux. https://github.com/iberianpig/fusuma
+
+sudo gpasswd -a $USER input
+newgrp input
+
+sudo apt install -y libinput-tools libevdev-dev ruby-dev
+sudo apt install -y ruby
+sudo gem install -y fusuma
+gsettings set org.gnome.desktop.peripherals.touchpad send-events enabled
+
+# install fusuma-plugin-sendkey. https://github.com/iberianpig/fusuma-plugin-sendkey
+sudo gem install revdev
+sudo gem install bundler
+sudo gem install fusuma-plugin-sendkey
+
+# copy configuration files
+mkdir -p ~/.config/fusuma
+cp -r ~/.local/share/omakub/configs/fusuma/* ~/.config/fusuma/
+
+# copy autostart file
+mkdir -p ~/.config/autostart/
+cp ~/.local/share/omakub/configs/autostart/fusuma.desktop ~/.config/autostart/fusuma.desktop
