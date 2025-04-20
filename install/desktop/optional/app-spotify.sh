962bc91ff048bfcfaa6f6b16e7c10c32d0db269f
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-04-20 19:27:56 -0400

    add spotify ad block

--- install/desktop/optional/app-spotify.sh
+++ install/desktop/optional/app-spotify.sh
@@ -3,3 +3,28 @@ curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo 
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update -y
 sudo apt install -y spotify-client
+
+cd /tmp
+git clone https://github.com/abba23/spotify-adblock.git
+cd spotify-adblock
+make
+sudo make install
+cd ..
+rm -rf spotify-adblock
+cd -
+
+mkdir -p ~/.local/share/applications
+
+cat > ~/.local/share/applications/spotify-adblock.desktop <<EOF
+[Desktop Entry]
+Type=Application
+Name=Spotify (adblock)
+GenericName=Music Player
+Icon=spotify-client
+TryExec=spotify
+Exec=env LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify %U
+Terminal=false
+MimeType=x-scheme-handler/spotify;
+Categories=Audio;Music;Player;AudioVideo;
+StartupWMClass=spotify
+EOF
