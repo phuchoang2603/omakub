ed756fdfd4bcf5b2d89fd63bdd7c3d646a57d41c
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-04-20 20:51:41 -0400

    feat: add installation of extension dependencies for ulauncher

--- install/desktop/ulauncher.sh
+++ install/desktop/ulauncher.sh
@@ -5,6 +5,17 @@ echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] http://p
 sudo apt update -y
 sudo apt install -y ulauncher
 
+# Install extension dependencies
+# for media-player-control
+sudo apt install playerctl
+# for calculate-anything
+sudo pip3 install Pint simpleeval parsedatetime pytz babel --break-system-packages
+# for firefox brotab
+sudo pip3 install memoization==0.4.0 brotab==1.4.2 --break-system-packages
+bt install
+# for vscode-recent
+sudo pip3 install fuzzywuzzy --break-system-packages
+
 # Start ulauncher to have it populate config before we overwrite
 mkdir -p ~/.config/autostart/
 cp ~/.local/share/omakub/configs/autostart/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
