d67f3207fe8950708c1ddc520c7e6d783e41dbdb
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-04-20 20:57:11 -0400

    fix: remove sudo from pip3 install commands and add -y flag to apt install for playerctl

--- install/desktop/ulauncher.sh
+++ install/desktop/ulauncher.sh
@@ -7,14 +7,14 @@ sudo apt install -y ulauncher
 
 # Install extension dependencies
 # for media-player-control
-sudo apt install playerctl
+sudo apt install -y playerctl
 # for calculate-anything
-sudo pip3 install Pint simpleeval parsedatetime pytz babel --break-system-packages
+pip3 install Pint simpleeval parsedatetime pytz babel --break-system-packages
 # for firefox brotab
-sudo pip3 install memoization==0.4.0 brotab==1.4.2 --break-system-packages
+pip3 install memoization==0.4.0 brotab==1.4.2 --break-system-packages
 bt install
 # for vscode-recent
-sudo pip3 install fuzzywuzzy --break-system-packages
+pip3 install fuzzywuzzy --break-system-packages
 
 # Start ulauncher to have it populate config before we overwrite
 mkdir -p ~/.config/autostart/
