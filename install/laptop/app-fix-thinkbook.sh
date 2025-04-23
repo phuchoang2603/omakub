17606241caef3cd5528dbd1673af7e6480fc776c
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-04-23 08:19:13 -0400

    add laptop conservation mode

--- install/laptop/app-fix-thinkbook.sh
+++ install/laptop/app-fix-thinkbook.sh
@@ -1,10 +1,13 @@
+# Enable Lenovo Conservation mode
+echo 1 | sudo tee /sys/bus/platform/devices/VPC2004:00/conservation_mode
+
+# Fix kernels
 # https://github.com/ferstar/ideapad-laptop-tb
 sudo apt install -y dkms
 
 mkdir -p ~/repos/
 git clone https://github.com/ferstar/ideapad-laptop-tb.git ~/repos/ideapad-laptop-tb
 cd ~/repos/ideapad-laptop-tb
-
 
 git reset --hard 45b1dc1
 make sync-source apply-patch
@@ -13,3 +16,4 @@ sudo dkms install ideapad-laptop-tb/6.10
 sudo cp dkms/blacklist-ideapad-laptop-tb-dkms.conf /etc/modprobe.d/
 
 cd -
+
