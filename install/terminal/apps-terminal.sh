ce0db6c1aa65cd20da3f01d7c0b45f598087d874
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-04-26 10:17:27 -0400

    minor tweaks

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,3 +1,1 @@
-sudo apt install -y fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr
-
-ln -s /usr/bin/batcat ~/.local/bin/bat
+sudo apt install -y fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr jq
--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -4,7 +4,6 @@ sudo apt install -y \
   libvips imagemagick libmagickwand-dev mupdf mupdf-tools \
   gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
   redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common \
-  python3-pip pipx \
-  jq
+  python3-pip pipx
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
