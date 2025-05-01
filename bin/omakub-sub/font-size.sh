c5ccf9006618ab005bc8482151a5a70776aff489
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-01 10:40:25 -0400

    refactor omakub bin

--- bin/omakub-sub/font-size.sh
+++
@@ -1,8 +0,0 @@
-choice=$(gum choose {7..14} "<< Back" --height 11 --header "Choose your terminal font size")
-
-if [[ $choice =~ ^[0-9]+$ ]]; then
-  sed -i "s/^font_size .*/font_size $choice/g" ~/.config/kitty/font-size.conf
-  source $OMAKUB_PATH/bin/omakub-sub/font-size.sh
-else
-  source $OMAKUB_PATH/bin/omakub-sub/font.sh
-fi
