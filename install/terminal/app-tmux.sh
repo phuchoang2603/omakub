25d7174922651cccbd205d928f959003a2fb8975
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-02 06:39:48 -0400

    fix tmux check

--- install/terminal/app-tmux.sh
+++ install/terminal/app-tmux.sh
@@ -1,7 +1,10 @@
 paru -S --noconfirm --needed tmux
 
 # Install TPM (Tmux Plugin Manager)
-git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
-
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+else
+  echo "TPM is already installed at $HOME/.tmux/plugins/tpm"
+fi
 go install github.com/joshmedeski/sesh/v2@latest
 go install github.com/arl/gitmux@latest
