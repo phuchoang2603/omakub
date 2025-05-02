e26337c82ae108af56f9a745f4b0969eb31d1848
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-02 07:03:46 -0400

    fix fusuma

--- install/desktop/gnome/app-fusuma.sh
+++ install/desktop/gnome/app-fusuma.sh
@@ -1,15 +1,9 @@
 # Fusuma is a multi-touch gesture recognizer for Linux. https://github.com/iberianpig/fusuma
 
 sudo gpasswd -a $USER input
-sg input <<EOF
-sudo gem install fusuma
-gsettings set org.gnome.desktop.peripherals.touchpad send-events enabled
+paru -S --noconfirm --needed ruby-fusuma ruby-fusuma-plugin-sendkey
 
-# install fusuma-plugin-sendkey. https://github.com/iberianpig/fusuma-plugin-sendkey
-sudo gem install revdev
-sudo gem install bundler
-sudo gem install fusuma-plugin-sendkey
-EOF
+gsettings set org.gnome.desktop.peripherals.touchpad send-events enabled
 
 # Run patch-gnome-shell-finger-count.py
 sudo python3 ~/.config/fusuma/patch-gnome-shell-finger-count.py
