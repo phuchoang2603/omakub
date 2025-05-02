5bf26a5245e0d1c7f60efae18f27a3bc91b8a5c6
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-02 05:54:33 -0400

    no need to sleep

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,15 +1,7 @@
 # Desktop software and tweaks will only be installed if we're running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
-  # Ensure computer doesn't go to sleep or lock while installing
-  gsettings set org.gnome.desktop.screensaver lock-enabled false
-  gsettings set org.gnome.desktop.session idle-delay 0
-
   for installer in ~/.local/share/omakub/install/desktop/*.sh; do source $installer; done
   for installer in ~/.local/share/omakub/install/desktop/gnome/*.sh; do source $installer; done
-
-  # Revert to normal idle and lock settings
-  gsettings set org.gnome.desktop.screensaver lock-enabled true
-  gsettings set org.gnome.desktop.session idle-delay 300
 fi
 
 # Logout to pickup changes
