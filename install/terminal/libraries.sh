e22a91ce3d4a458960a3e12cfc75b82065eb28db
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-02 01:40:09 -0400

    fix paru sudo permission

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -1,4 +1,4 @@
-sudo paru -S --noconfirm \
+paru -S --noconfirm \
   autoconf bison base-devel clang pkgconf \
   ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick \
   bat btop eza tldr \
--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,2 +1,2 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
-sudo paru -S gum
+paru -S --noconfirm gum
