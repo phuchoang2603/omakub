8deb30ad4353f8ba0f6c3b39084c9089b7a36765
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-02 05:59:02 -0400

    fix docker logs

--- install/terminal/app-docker.sh
+++ install/terminal/app-docker.sh
@@ -6,6 +6,3 @@ sudo systemctl start docker.service
 
 # Give this user privileged Docker access
 sudo usermod -aG docker ${USER}
-
-# Limit log size to avoid running out of disk
-echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
