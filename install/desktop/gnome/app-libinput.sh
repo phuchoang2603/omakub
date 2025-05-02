8ba4b97f57367c9f86605381b50b939340b304c2
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-02 07:04:36 -0400

    fix libinput clone

--- install/desktop/gnome/app-libinput.sh
+++ install/desktop/gnome/app-libinput.sh
@@ -1,7 +1,11 @@
 # Libinput is a library that handles input devices in Wayland and X11. Fix touchpad scrolling way too fast
 
 mkdir -p ~/repos/
-git clone https://gitlab.com/warningnonpotablewater/libinput-config.git ~/repos/libinput-config
+if [ ! -d "$HOME/repos/libinput-config" ]; then
+  git clone https://gitlab.com/warningnonpotablewater/libinput-config.git "$HOME/repos/libinput-config"
+else
+  echo "Libinput Config is already cloned at $HOME/repos/libinput-config"
+fi
 
 sudo cp ~/.config/libinput.conf /etc/libinput.conf
 
