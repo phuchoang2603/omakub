97c6a86c1d181ba019cab2f6fd3cd1e0b05cce16
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-02 20:06:48 -0400

    remove extensiosn

--- install/desktop/gnome/app-ulauncher.sh
+++ install/desktop/gnome/app-ulauncher.sh
@@ -3,7 +3,3 @@ paru -S --noconfirm --needed ulauncher
 # Install extension dependencies
 # for media-player-control
 paru -S --noconfirm --needed playerctl
-# for calculate-anything
-pip3 install Pint simpleeval parsedatetime pytz babel --break-system-packages
-# for vscode-recent
-pip3 install fuzzywuzzy --break-system-packages
