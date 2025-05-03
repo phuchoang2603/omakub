4fb253c96bc1522745d1092714895f1748384d7a
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-03 19:49:17 -0400

    remove sddm and add option to choose theme and font when instsall

--- install/desktop.sh
+++ install/desktop.sh
@@ -4,5 +4,8 @@ if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   for installer in ~/.local/share/omakub/install/desktop/gnome/*.sh; do source $installer; done
 fi
 
+source ~/.local/share/omakub/bin/change-theme
+source ~/.local/share/omakub/bin/change-font
+
 # Logout to pickup changes
 gum confirm "Ready to reboot for all settings to take effect?" && sudo reboot
