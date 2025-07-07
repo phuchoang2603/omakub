551b27caed4b3ffb0455cfc48975d07749b6bdec
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-07-07 18:19:43 +0700

    add pywalfox for pywal firefox integration

--- install/desktop/app-pywal.sh
+++ install/desktop/app-pywal.sh
@@ -1,7 +1,7 @@
 pipx install pywal16
 
-# pipx install pywalfox
+pipx install pywalfox
 
-# pywalfox install
-#
-# find ~/.local/share/pipx/venvs/pywalfox -type f -name "main.sh" -exec sed -i.bak 's#^python -m pywalfox start.*#pywalfox start || &#' {} +
+pywalfox install
+
+find ~/.local/share/pipx/venvs/pywalfox -type f -name "main.sh" -exec sed -i.bak 's#^python -m pywalfox start.*#pywalfox start || &#' {} +
