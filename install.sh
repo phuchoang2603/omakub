eac91a5f63962859f3143a80c8a6fda08f4b5263
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-07-09 20:51:45 +0700

    remove error exit stuff

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 # Log all output to a file
 exec > >(tee -a ~/.local/share/omakub/install.log) 2>&1
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
 source ~/.local/share/omakub/check-os.sh
 
 read -p "Enter github username: " OMAKUB_USER_NAME
