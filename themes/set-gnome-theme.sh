e21dd6219c4b5c1bf2cdc2180566c47401b2eadb
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-07-09 16:42:38 +0700

    update gnome theme for papirus

--- themes/set-gnome-theme.sh
+++ themes/set-gnome-theme.sh
@@ -1,8 +1,18 @@
 # Set GNOME UI theme preferences
-gsettings set org.gnome.desktop.interface gtk-theme "Graphite-${OMAKUB_THEME_COLOR}-Dark"
-papirus-folders -C "$OMAKUB_THEME_COLOR"
+gsettings set org.gnome.desktop.interface gtk-theme "Graphite-Dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+
+# Update papirus icon cache in background to avoid blocking the terminal
+CACHE_UPDATE_SCRIPT="/tmp/update-cache.sh"
+LOG_FILE="/tmp/update-cache.log"
+cat >"$CACHE_UPDATE_SCRIPT" <<EOF
+#!/bin/bash
+set -e
+papirus-folders -C "$OMAKUB_THEME_COLOR" --theme Papirus-Dark
+EOF
+chmod +x "$CACHE_UPDATE_SCRIPT"
+nohup "$CACHE_UPDATE_SCRIPT" &>"$LOG_FILE" &
 
 # Wallpaper setup
 BACKGROUND_ORG_PATH="$HOME/.local/share/omakub/themes/$OMAKUB_THEME_BACKGROUND"
