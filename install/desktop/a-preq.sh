94c7b8e8e5bf4e0f9b598f70a515d12979e89507
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-07-23 21:15:25 +0700

    zen to edge browser

--- install/desktop/a-preq.sh
+++ install/desktop/a-preq.sh
@@ -1,9 +1,13 @@
 paru -S --noconfirm --needed flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-paru -S --noconfirm --needed brightnessctl wl-clipboard bluez bluez-utils playerctl vlc pavucontrol wireplumber tumbler
+paru -S --noconfirm --needed brightnessctl wl-clipboard bluez bluez-utils playerctl mpv pavucontrol wireplumber tumbler
 sudo systemctl enable --now bluetooth.service
 
 # brotab
 pipx install brotab
 bt install
+
+# ff2mpv
+git clone https://github.com/woodruffw/ff2mpv.git --depth 1 ~/repos/ff2mpv
+source ~/repos/ff2mpv/install.sh firefox
--- install/optional/app-microsoft-edge.sh
+++ install/optional/app-microsoft-edge.sh
@@ -1,1 +1,1 @@
-paru -S --noconfirm --needed microsoft-edge-stable-bin
+paru -S --noconfirm --needed microsoft-edge-beta-bin profile-sync-daemon-edge
