8ad254a9ebe0eed6791add9f5eac84638bd71b71
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-09-13 13:30:00 -0400

    add blueman

--- install/desktop/a-preq.sh
+++ install/desktop/a-preq.sh
@@ -1,7 +1,7 @@
 paru -S --noconfirm --needed flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-paru -S --noconfirm --needed brightnessctl wl-clipboard bluez bluez-utils playerctl mpv pavucontrol wireplumber tumbler
+paru -S --noconfirm --needed brightnessctl wl-clipboard bluez bluez-utils blueman playerctl mpv pavucontrol wireplumber tumbler
 sudo systemctl enable --now bluetooth.service
 
 # ff2mpv
