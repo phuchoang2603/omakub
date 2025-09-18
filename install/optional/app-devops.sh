af50d578d35f3e1558c8069cf07eef1f8fa69460
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-09-18 08:17:59 -0400

    add krew plugin

--- install/optional/app-devops.sh
+++ install/optional/app-devops.sh
@@ -4,8 +4,20 @@ if ! command -v kubectl &>/dev/null; then
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
   cd - || return 1
+
+  # Installing krew, a plugin manager for kubectl
+  (
+    set -x
+    cd "$(mktemp -d)" &&
+      OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+      ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+      KREW="krew-${OS}_${ARCH}" &&
+      curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+      tar zxvf "${KREW}.tar.gz" &&
+      ./"${KREW}" install krew
+  )
 else
-  echo "✅ kubectl is already installed."
+  echo "✅ kubectl and krew is already installed."
 fi
 
 if ! command -v helm &>/dev/null; then
