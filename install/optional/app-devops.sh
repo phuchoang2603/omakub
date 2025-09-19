bc582474ce477ea3e4cb936d705a770fcd2cf02a
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-09-18 20:34:43 -0400

    add aws package

--- install/optional/app-devops.sh
+++ install/optional/app-devops.sh
@@ -40,4 +40,8 @@ else
   echo "✅ kubectx is already installed."
 fi
 
+# Google Cloud SDK
 paru -S --noconfirm --needed google-cloud-cli-lite google-cloud-cli-gke-gcloud-auth-plugin
+
+# AWS CLI
+paru -S --noconfirm --needed aws-cli-v2
