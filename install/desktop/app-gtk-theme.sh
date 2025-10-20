97d1f9fe8c55b01389718256f5dfcdf986a566aa
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-10-20 02:41:59 -0400

    fix icon not installed

--- install/desktop/app-gtk-theme.sh
+++ install/desktop/app-gtk-theme.sh
@@ -14,7 +14,7 @@ packages=(
 paru -S --noconfirm --needed "${packages[@]}"
 
 if [ ! -d ~/repos/Graphite-gtk-theme ]; then
-  wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.icons" sh
+  wget -qO- https://git.io/papirus-icon-theme-install | sh
   wget -qO- https://git.io/papirus-folders-install | env PREFIX=$HOME/.local sh
   git clone https://github.com/vinceliuice/Graphite-gtk-theme.git --depth 1 ~/repos/Graphite-gtk-theme
 else
