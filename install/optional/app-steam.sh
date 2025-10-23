35950fd290c8ec0a7bc040b222ef2d47c25c9aec
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-10-23 02:00:45 -0400

    remove steam grib db

--- install/optional/app-steam.sh
+++ install/optional/app-steam.sh
@@ -10,10 +10,3 @@ paru -Syy
 
 echo "=> Installing Steam..."
 paru -S --noconfirm --needed steam glxinfo
-
-# Install SteamGridDB
-wget https://github.com/SteamGridDB/SGDBoop/releases/latest/download/sgdboop-linux64.tar.gz
-mkdir sgdboop-linux64
-tar -zxf sgdboop-linux64.tar.gz -C sgdboop-linux64
-chmod +x sgdboop-linux64/install.sh
-sudo ./sgdboop-linux64/install.sh
