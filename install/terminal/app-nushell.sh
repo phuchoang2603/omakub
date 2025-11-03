293c3a80edef7e262da2f1fa3df1ca276dbf23d9
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-11-03 14:45:00 -0500

    add bash env plugin

--- install/terminal/app-nushell.sh
+++ install/terminal/app-nushell.sh
@@ -1,8 +1,12 @@
 if [ "$OMAKUB_OS_ID" == "arch" ]; then
+  # Nushell & carapace-bin
   paru -S --noconfirm --needed nushell carapace-bin
+  # Nufmt
   cargo install --git https://github.com/nushell/nufmt
+  # Bash-env plugin
+  wget -O ~/.local/bin/bash-env-json "https://raw.githubusercontent.com/tesujimath/bash-env-json/main/bash-env-json" && chmod +x ~/.local/bin/bash-env-json
 elif [ "$OMAKUB_OS_ID" == "ubuntu" ]; then
-  # Nushell
+  # Nushell & carapace-bin
   curl -fsSL https://apt.fury.io/nushell/gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/fury-nushell.gpg
   sudo tee /etc/apt/sources.list.d/fury.list <<EOF
 deb https://apt.fury.io/nushell/ /
@@ -11,5 +15,8 @@ EOF
 
   sudo apt update -y
   sudo apt install -y nushell carapace-bin
+  # Nufmt
   cargo install --git https://github.com/nushell/nufmt
+  # Bash-env plugin
+  wget -O ~/.local/bin/bash-env-json "https://raw.githubusercontent.com/tesujimath/bash-env-json/main/bash-env-json" && chmod +x ~/.local/bin/bash-env-json
 fi
