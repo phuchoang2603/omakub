d06401ef3c678acc37ec6049eeb959f15fd7cec5
felix <xuanphuc.a1gv@gmail.com>
2026-01-18 21:23:45 -0500

    switch to microsoft edge stable

--- install/desktop/app-browser.sh
+++ install/desktop/app-browser.sh
@@ -1,18 +1,5 @@
-paru -S --noconfirm --needed microsoft-edge-beta-bin
-# bash <(curl -s https://updates.zen-browser.app/install.sh)
+paru -S --noconfirm --needed microsoft-edge-stable-bin
 
 # Preload is a daemon that preloads applications into memory to speed up their launch time
 paru -S --noconfirm --needed preload
 sudo systemctl enable --now preload.service
-
-# Profile-sync-daemon is a tool that allows browsers to use RAM for their profile storage, improving performance
-paru -S --noconfirm --needed profile-sync-daemon profile-sync-daemon-edge
-# paru -S --noconfirm --needed profile-sync-daemon-zen
-
-USERNAME="$USER"
-SUDOERS_FILE="/etc/sudoers.d/99-psd-helper-$USERNAME"
-
-echo "$USERNAME ALL=(ALL) NOPASSWD: /usr/bin/psd-overlay-helper" | sudo tee "$SUDOERS_FILE" >/dev/null
-sudo chmod 440 "$SUDOERS_FILE"
-
-systemctl --user enable --now psd.service
--- install/desktop/gnome/set-gnome-zdock.sh
+++ install/desktop/gnome/set-gnome-zdock.sh
@@ -1,6 +1,6 @@
 # Favorite apps for dock
 apps=(
-  "microsoft-edge-beta.desktop"
+  "microsoft-edge.desktop"
   "kitty.desktop"
   # "Neovim.desktop"
   "code.desktop"
@@ -39,4 +39,4 @@ favorites_list="[${favorites_list%,}]"
 gsettings set org.gnome.shell favorite-apps "$favorites_list"
 
 # Configure Auto Move Windows
-gsettings set org.gnome.shell.extensions.auto-move-windows application-list "['microsoft-edge-beta.desktop:1', 'kitty.desktop:2', 'code.desktop:3', 'spotify-launcher.desktop:4', 'md.obsidian.Obsidian.desktop:5', 'org.gnome.Nautilus.desktop:6']"
+gsettings set org.gnome.shell.extensions.auto-move-windows application-list "['microsoft-edge.desktop:1', 'kitty.desktop:2', 'code.desktop:3', 'spotify-launcher.desktop:4', 'md.obsidian.Obsidian.desktop:5', 'org.gnome.Nautilus.desktop:6']"
